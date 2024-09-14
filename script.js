b23700282598c3bca851235de234d9626b7919f1
Amit_Valand <143027284+Amitvaland14@users.noreply.github.com>
2024-09-14 23:18:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const daysEl = document.getElementById("days")
+const hoursEl = document.getElementById("hours")
+const minsEl = document.getElementById("mins")
+const secEl = document.getElementById("sec")
+
+const birthday = "16 Sep 2024";
+
+function countdown(){
+    const birthDayDate = new Date(birthday);
+    const currentDate = new Date();
+
+    const totalSeconds = (birthDayDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds /3600/24);
+    const hours = Math.floor(totalSeconds / 3600 )%24;
+    const mins = Math.floor(totalSeconds / 60) %60;
+    const seconds = Math.floor(totalSeconds)%60;
+    //console.log(days, hours, mins, seconds);
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = hours;
+    minsEl.innerHTML = mins;
+    secEl.innerHTML = seconds;
+}
+countdown();
+setInterval(countdown, 1000);
